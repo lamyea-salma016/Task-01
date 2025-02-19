cab340095bfa0846bdba01dc5ee5635c66ecce7e
lamyea-salma016 <lamyeasalma147@gmail.com>
2025-02-19 11:01:29 +0600

    Task 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyWidget());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI-Flutter",
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Basic Flutter"),
-            backgroundColor: const Color.fromARGB(255, 167, 35, 79),
-            foregroundColor: const Color.fromARGB(255, 196, 208, 26),
-            centerTitle: true,
-            elevation: 20.5,
-            shadowColor: Colors.blue,
-          ),
-          body: Row(
-            children: [
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('MyWidget'),
+          centerTitle: true,
+        ),
+        body: Container(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
               Icon(
                 Icons.star,
                 size: 60,
-                color: const Color.fromARGB(255, 214, 228, 16),
+                color: Colors.yellow,
               ),
-               Icon(
+              Image(
+                image: AssetImage('assets/Shoes.png'),
+                width: 100,
+                height: 100,
+              ),
+              Icon(
                 Icons.star,
+                weight: 50,
                 size: 60,
-                color: const Color.fromARGB(255, 214, 228, 16),
+                color: Colors.yellow,
               ),
-                Icon(
-                  Icons.star,
-                  size: 60,
-                  color: const Color.fromARGB(255, 22, 30, 180),
-                ),
-                Icon(
-                  Icons.star,
-                  size: 60,
-                  color: const Color.fromARGB(255, 173, 20, 20),
-                ),
-                Icon(
-                  Icons.star,
-                  size: 60,
-                  color: const Color.fromARGB(255, 190, 25, 25),
-                ),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
